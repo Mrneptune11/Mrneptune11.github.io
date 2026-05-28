5ae016d18af5844d1c6bbdb6e6710627663ca9ef
Sebastian Lanz <sebastianlanz301@gmail.com>
2026-05-28 17:13:23 -0500

    test

--- src/TS/main_pages/projects.tsx
+++ src/TS/main_pages/projects.tsx
@@ -36,6 +36,7 @@ function Projects() {
                     <HappyHelpersCard/>
                     <ThymeTimeCard/>
                     <ProjectSpookems/>
+                    <ProjectSpookems/>
                 </Container>
         </div>
     )
